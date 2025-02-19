3c616deb85006a6c8d4b502ee19488b23eab51bc
Minsun Kim <msggms1@gmail.com>
2025-02-19 17:41:16 +0900

    days13 수업

--- javaPro/src/days13/달팽이.java
+++ javaPro/src/days13/달팽이.java
@@ -3,8 +3,41 @@ package days13;
 public class 달팽이 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			// 정처기 실기
+			int [][] m = new int [5][5];
 
+			fillM04(m);
+			dispM(m);
+
+
+		} // main
+
+	
+
+
+
+
+	private static void dispM(int[][] m) {
+		System.out.println("      0열  1열  2열  3열  4열");
+		
+		for (int i = 0; i < m.length; i++) {
+			System.out.printf("[%d행]", i);
+			for (int j = 0; j < m[i].length; j++) {
+				System.out.printf("[%2d]", m[i][j]);
+			} // for j
+			System.out.println();
+		} // for i
+		
 	}
 
-}
+	private static void fillM04(int[][] m) {
+		for (int i = 0 ; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				m[j][4-i] = 5*i+j+1;
+			} // for j 00 11
+			System.out.println();
+		} // for i
+		
+	} // main
+
+} // class
