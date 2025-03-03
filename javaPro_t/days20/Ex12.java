fa7450de1cb9d98faeeaf0fa64df75bbddaa3447
Minsun Kim <msggms1@gmail.com>
2025-03-03 23:25:56 +0900

    .

--- javaPro_t/days20/Ex12.java
+++ javaPro_t/days20/Ex12.java
@@ -1,6 +1,9 @@
 package days20;
 
 import java.text.ChoiceFormat;
+import java.text.DecimalFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author kenik
@@ -14,23 +17,21 @@ import java.text.ChoiceFormat;
 public class Ex12 {
 
 	public static void main(String[] args) {
-		int [] kors = { 100, 57,  95, 88, 77, 80, 0 };
+
 		
-		/* [1]
-		//               낮은 값부터
-		double [] limits = {0, 60, 70, 80, 90};
-		String [] formats = {"가","양", "미", "우","수"};
-		ChoiceFormat cf = new ChoiceFormat(limits, formats);
-		*/
-		
-		// [2]
-		String newPattern = "0#가|60#양|70#미|80#우|90#수";
-		ChoiceFormat cf = new ChoiceFormat(newPattern);
-		
-	    for (int i = 0; i < kors.length; i++) {
-			System.out.printf("%d점수 - %s 등급\n", kors[i]
-					,  cf.format(kors[i]) );
-		} // for i
+		String source = "Java is fun and Java is powerful!";
+		String pattern = "Java";
+
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(source);
+		StringBuffer sb = new StringBuffer();
+
+		while (m.find()) {
+		    m.appendReplacement(sb, "Python");
+		    m.appendTail(sb); 
+		}
+
+		System.out.println(sb.toString());
 
 	} // main
 
