c250fe17ac18430f9e0ecdddb4dc63b706920155
Minsun Kim <msggms1@gmail.com>
2025-03-17 11:23:11 +0900

    .

--- javaPro_t/days28/Ex04_04.java
+++ javaPro_t/days28/Ex04_04.java
@@ -12,19 +12,18 @@ public class Ex04_04 {
 		// Stream<Path> Files.list(Path dir)
 		// Stream<String> Files.lines(Path )
 		// Stream<String> BufferedReader.lines()
-		
+
 		String uri = ".\\src\\days28\\Ex01.java"; 
 		// 라인단위로 읽어와서 출력(처리)
 		// FileReader , BufferedReader,   String line, while(line != null){ }
-		
+
 		// Path path = Path.of(URI uri);
 		// Path path = Paths.get(uri);
-		Path path = Path.of(uri );
-		Stream<String> lines = Files.lines(path );
-	lines.forEach(System.out::println);	
+		Path path = Path.of( uri );
+		Stream<String> lines = Files.lines( path );
+		lines.forEach(System.out::println);	
 		
 
 	} // main
 
 }
-  
